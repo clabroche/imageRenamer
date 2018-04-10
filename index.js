fb3eb77329f40d7209cd27e4615896c089e62379
labroche <corentin.labroche@inist.fr>
2018-04-10 22:31:37 +0200

    fix date + output no date

--- index.js
+++ index.js
@@ -10,38 +10,42 @@ const delimiter = '_';
     if (!fse.existsSync(directory)) throw new Error('path doesn\'t exist')
     const files = await fse.readdir(directory)
     const outputFiles = pathfs.resolve(directory, 'out')
-    const problemPath = pathfs.resolve(outputFiles, 'problem')
+    const problemPath = pathfs.resolve(outputFiles, 'No Date')
     mkdirp.sync(outputFiles)
-    const a = await Promise.map(files, file=>{
+    const a = await Promise.map(files, async file=>{
         const path = pathfs.resolve(directory,file)
         if (fse.lstatSync(path).isDirectory()) return
-        return exif.read(path).then(data=>{
-            let dateString            
-            if (data !== null  && data.hasOwnProperty("exif") && data.exif && data.exif.hasOwnProperty('DateTimeOriginal')) {
-                dateString = data.exif.DateTimeOriginal
-            } else if (data !== null  && data.hasOwnProperty("image") && data.image && data.image.hasOwnProperty('ModifyDate')) {
-                console.log("Take the modify date:",path)
-                dateString = data.image.ModifyDate;
-            } else {
-                console.log('no exif', path)
-                if (fse.existsSync(pathfs.resolve(problemPath, file))) {
-                    console.log(pathfs.resolve(problemPath, file), "exist !");
-                }
-                return fse.copy(path, pathfs.resolve(problemPath, file), {overwrite:false})
+        const data = await exif.read(path)
+        let dateString
+        if (data !== null  && data.hasOwnProperty("exif") && data.exif && data.exif.hasOwnProperty('DateTimeOriginal')) {
+            dateString = data.exif.DateTimeOriginal
+        } else if (data !== null  && data.hasOwnProperty("image") && data.image && data.image.hasOwnProperty('ModifyDate')) {
+            console.log("Take the modify date:",path)
+            dateString = data.image.ModifyDate;
+        } else {
+            console.log('no exif', path)
+            if (fse.existsSync(pathfs.resolve(problemPath, file))) {
+                console.log(pathfs.resolve(problemPath, file), "exist !");
             }
-            const date = new Date(dateString)
-            const newName = 
-                date.getFullYear() + delimiter + 
-                date.getMonth() + delimiter + 
-                date.getDate() + delimiter + 
-                date.getHours() + "" + 
-                date.getMinutes() + "" + 
-                date.getSeconds() + "" + 
-                (Math.random() *1000).toFixed(0)
-            if (fse.existsSync(pathfs.resolve(outputFiles, newName + pathfs.extname(file)))) {
-              console.log(pathfs.resolve(outputFiles, newName + pathfs.extname(file)), "exist !: ", path);
-            }
-            return fse.copy(path, pathfs.resolve(outputFiles, newName + pathfs.extname(file)), {overwrite:false})
-        }).catch(console.error);
+            return fse.copy(path, pathfs.resolve(problemPath, file), {overwrite:false})
+        }
+        const date = new Date(dateString)
+        const month = date.getUTCMonth() + 1 < 10 ? `0${date.getUTCMonth() + 1}` : date.getUTCMonth() + 1;
+        const day = date.getUTCDate() < 10 ? `0${date.getUTCDate()}` : date.getUTCDate(); 
+        const hours = date.getUTCHours() < 10 ? `0${date.getUTCHours()}` : date.getUTCHours(); 
+        const minutes = date.getUTCMinutes() < 10 ? `0${date.getUTCMinutes()}` : date.getUTCMinutes(); 
+        const seconds = date.getUTCSeconds() < 10 ? `0${date.getUTCSeconds()}` : date.getUTCSeconds();
+        const newName = 
+            date.getUTCFullYear() + "-" + 
+            month + "-" + 
+            day + "_" + 
+            hours + ":" + 
+            minutes+ ":" + 
+            seconds + '_' +
+            (Math.random() *1000).toFixed(0)
+        if (fse.existsSync(pathfs.resolve(outputFiles, newName + pathfs.extname(file)))) {
+            console.log(pathfs.resolve(outputFiles, newName + pathfs.extname(file)), "exist !: ", path);
+        }
+        return fse.copy(path, pathfs.resolve(outputFiles, newName + pathfs.extname(file)), {overwrite:false})
     },{concurrency:10})
 })().catch(err=>console.log('err:', err))
