995d292729bf4fdecad2a654f5d6acf63afb2d50
clabroche <corentinlabroche@gmail.com>
2021-06-17 00:12:50 +0200

    fix(copy): can organize in days folder

--- app.js
+++ app.js
@@ -1,6 +1,6 @@
 const path = require('path')
 const ImgRenamer = require('./index')
-const imgRenamer = new ImgRenamer({
+new ImgRenamer({
     inputDirectory: path.resolve(process.argv[2]) || path.resolve('./')
 }).launch().then(data=>{
     console.log(data)
--- index.js
+++ index.js
@@ -3,7 +3,8 @@ const Promise = require("bluebird");
 const pathfs = require("path");
 var exif = require("fast-exif");
 var mkdirp = require("mkdirp");
-const {prompt} = require('inquirer')
+const inquirer = require('inquirer');
+const { basename, dirname } = require("path");
 const delimiter = "_";
 
 function imageRenamer(config = {}) {
@@ -18,10 +19,26 @@ imageRenamer.prototype.launch = async function() {
   const outputFiles = this.outputDirectory
   mkdirp.sync(outputFiles);
   const files = await fse.readdir(directory);
-  await Promise
-    .map(files, file => this.analyzeFile(pathfs.resolve(directory, file)),{ concurrency: 10 })
-    .map(copy, {concurrency: 10});
+  const filesInfos = await Promise.map(files, file => this.analyzeFile(pathfs.resolve(directory, file)),{ concurrency: 10 })
+  await this.menu(filesInfos)
 };
+
+imageRenamer.prototype.menu = async function(filesInfos) {
+  console.log('All files has been analyzed')
+  const {fun} = await inquirer.prompt({ name: 'Choisissez', message:'', type: 'list', name: 'fun',choices: [
+    { type: 'choice', value: this.copyAll, name: 'Rename all files' },
+    { type: 'choice', value: this.copyAllAndDaysSplit, name: 'Rename all files and place it in days folder' },
+  ]})
+  return fun(filesInfos)
+}
+
+imageRenamer.prototype.copyAll = function (filesInfos) {
+  return Promise.map(filesInfos, copyFile, { concurrency: 10 });
+}
+
+imageRenamer.prototype.copyAllAndDaysSplit = function (filesInfos) {
+  return Promise.map(filesInfos, (file) => copyFile(file, {inDaysFolder: true}), { concurrency: 10 });
+}
 
 imageRenamer.prototype.analyzeFile = async function(path) {
   const filename = pathfs.basename(path)
@@ -29,30 +46,53 @@ imageRenamer.prototype.analyzeFile = async function(path) {
   const res = {
     path,
     newPath: '',
+    dates: {
+      date: null, year: null, month: null, day: null, hours: null, minutes: null, seconds:null
+    }
   }
   if (fse.lstatSync(path).isDirectory()) return res;
-
+  // Try to guess  dates from file
   const data = await readExif(path)
   const hasDateTimeOriginal = () => data !== null && data.hasOwnProperty("exif") && data.exif && data.exif.hasOwnProperty("DateTimeOriginal")
   const hasModifiedDate = () => data !== null && data.hasOwnProperty("image") && data.image && data.image.hasOwnProperty("ModifyDate")
-  if (hasDateTimeOriginal()) res.newPath = pathfs.resolve(this.outputDirectory, guessfilename(data.exif.DateTimeOriginal) + pathfs.extname(path));
-  else if (hasModifiedDate()) res.newPath = pathfs.resolve(this.outputDirectory, guessfilename(data.image.ModifyDate) + pathfs.extname(path));
+  if (hasDateTimeOriginal()) {
+    res.dates = getDates(data.exif.DateTimeOriginal)
+  } else if (hasModifiedDate()) {
+    res.dates = getDates(data.image.ModifyDate)
+  }
+  // Build new path with dates if available
+  if(res.dates.date) res.newPath = pathfs.resolve(this.outputDirectory, guessfilename(res.dates) + pathfs.extname(path));
   else res.newPath = problemPath
   return res
 }
-function guessfilename(dateString) {
+function getDates(dateString) {
   const date = new Date(dateString);
+  const year = date.getUTCFullYear()
   const month = date.getUTCMonth() + 1 < 10 ? `0${date.getUTCMonth() + 1}` : date.getUTCMonth() + 1;
   const day = date.getUTCDate() < 10 ? `0${date.getUTCDate()}` : date.getUTCDate();
   const hours = date.getUTCHours() < 10 ? `0${date.getUTCHours()}` : date.getUTCHours();
   const minutes = date.getUTCMinutes() < 10 ? `0${date.getUTCMinutes()}` : date.getUTCMinutes();
   const seconds = date.getUTCSeconds() < 10 ? `0${date.getUTCSeconds()}` : date.getUTCSeconds();
-  const newName = date.getUTCFullYear() + "-" + month + "-" + day + "_" + hours + "-" + minutes + "-" + seconds + "_" + (Math.random() * 1000).toFixed(0);
-  return newName
+  return {
+    date, year, month, day, hours,minutes,seconds
+  }
+}
+function guessfilename({year, month, day, hours, minutes, seconds}) {
+  return year + "-" + month + "-" + day + "_" + hours + "-" + minutes + "-" + seconds + "_" + (Math.random() * 1000).toFixed(0);
 }
 
-function copy(file) {
-  const newPath = file.newPath
+function copyFile(file, options = {}) {
+  let newPath = file.newPath
+  if (options.inDaysFolder && !newPath.includes('No Date') && file.dates.date) {
+    const { year, month, day } = file.dates
+    const folder = `${year}-${month}-${day}`
+    const dirPath = pathfs.resolve(dirname(newPath), folder)
+    if(!fse.existsSync(dirPath)) {
+      fse.mkdirpSync(dirPath)
+    }
+    newPath = pathfs.resolve(dirPath, basename(newPath))
+  }
+
   const path = file.path
   const exist = fse.existsSync(newPath)
   if (newPath && !exist) {
